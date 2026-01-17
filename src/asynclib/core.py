dcedf6a9fb143f6ca288f0299ae24b88c44d5147
Arsalan Arish <mail@arsalanarish.com>
2026-01-17 16:28:43 +0500

    Make everything OOPs friendly Now there is a single class 'Handler' which takes care of the protocol, queue, handling_commands, and other stuff. The event loop creates an instance of the handler for its operations

--- src/asynclib/core.py
+++ src/asynclib/core.py
@@ -15,67 +15,76 @@ __all__ = [
 ]
 
 
-#* Signal Command Handling functions
-def await_time(**kwargs):
-    #SPEC SIGNAL SPEC PARAMS --> {time: int}
-    time = kwargs["time"]
-    import time as t
-    t.sleep(time)
-    return None
-
-def await_coroutine(**kwargs):
-    #SPEC SIGNAL SPEC PARAMS --> {target: function_object, args : tuple, kwargs : dict}
-    target = kwargs.get("target") # It must be a generator object
-    if not target:
-        raise ValueError("ERROR : No target specified")
-
-    # Actual Action
-    loop = Event_Loop()
-    result = loop.run(target)
-    return result
-
-def create_task(**kwargs):
-    pass
-    return None
-
-def await_all_tasks(**kwargs):
-    pass
-
-def await_task(**kwargs):
-    pass
-
-def none(**kwargs):
-    return None
-
-#* Protocol dictionary contains all signals reference
-protocol = {
-    "await_time" : await_time,
-    "await_coroutine": await_coroutine,
-    "none": none,
-    "create_task": create_task,
-    "await_all_tasks": await_all_tasks,
-    "await_task": await_task,
-}
-
 #* Task_queue used by the event loop
-class task_queue:
+class Task_Queue:
     def __init__(self):
         self.queue = []
-    def enqueue(self, coro):
-        self.queue.append(coro)
-    def dequeue(self, coro):
-        self.queue.pop()
+    def enqueue(self, generator):
+        self.queue.append(generator)
+    def dequeue(self, generator):
+        self.queue.pop(0)
     
+
+#* Signal Command Handling functions
+class Handler:
+
+    def __init__(self):
+        self.queue = Task_Queue()
+        self.protocol = {
+        "await_time" : self.await_time,
+        "await_coroutine": self.await_coroutine,
+        "none": self.none,
+        "create_task": self.create_task,
+        "await_all_tasks": self.await_all_tasks,
+        "await_task": self.await_task,
+      }
+    
+
+    def await_time(self, **kwargs):
+        #SPEC SIGNAL SPEC PARAMS --> {time: int}
+        time = kwargs["time"]
+        import time as t
+        t.sleep(time)
+        return None
+
+    def await_coroutine(self, **kwargs):
+        #SPEC SIGNAL SPEC PARAMS --> {target: generator_object}
+        target = kwargs.get("target") # It must be a generator object
+        if not target:
+            raise ValueError("ERROR : No target specified")
+
+        loop = Event_Loop()
+        result = loop.run(target)
+        return result
+
+    def create_task(self, **kwargs):
+        #SPEC SIGNAL SPEC PARAMS --> {target: generator_object}
+
+        return None
+
+    def await_all_tasks(self, **kwargs):
+        #SPEC SIGNAL SPEC PARAMS --> {target: generator_object}
+        pass
+
+    def await_task(self, **kwargs):
+        #SPEC SIGNAL SPEC PARAMS --> {target: generator_object}
+        pass
+
+    def none(self, **kwargs):
+        #SPEC SIGNAL SPEC PARAMS --> {target: generator_object}
+        return None
+
+
 
 #* Custom event loop
 class Event_Loop:
     def __init__(self):
+        self.handler = Handler()
         self.__doc__ = f"""
         Signal SPEC --> tuple(<command>, dict(param1: arg1, param2, arg2, ...))
-        Protocol SPEC --> 
-        {list(protocol.keys())}
-    """
-
+        Protocol SPEC -->
+        {list(self.handler.protocol.keys())}"""
+        
     SIGNAL_SYNTAX_ERROR = "\nTHE EVENT LOOP CANNOT RECOGNIZE SUCH SYNTAX OF SIGNAL\n PLEASE OBEY THE 'SIGNAL SPEC'!"
     PROTOCOL_ERROR = "\nTHIS COMMAND IS NOT IN THE PROTOCOL\n PLEASE OBEY THE 'PROTOCOL'!"
 
@@ -92,9 +101,9 @@ class Event_Loop:
                 except ValueError:
                     raise ValueError(SIGNAL_SYNTAX_ERROR)
             # Handle the signal
-                if command in protocol:
+                if command in self.handler.protocol:
                     try:
-                        returned_value = protocol[command](**kwargs) # Calling the handling function
+                        returned_value = self.handler.protocol[command](**kwargs) # Calling the handling function
                         generator_sending_value = deepcopy(returned_value) # An arbitrary check just in case any bugs come
                     except Exception as e:
                         if command == "await_coroutine":
